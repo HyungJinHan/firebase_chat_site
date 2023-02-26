6d8a7b34129e64f466932e88eaeb2c4369398b42
HyungJinHan <han1210_36@naver.com>
2023-02-27 06:44:41 +0900

    컴포넌트 병합

--- renderer/pages/chat/chatbox.tsx
+++ renderer/pages/chat/chatbox.tsx
@@ -1,16 +1,26 @@
-import { collection, limit, onSnapshot, orderBy, query } from 'firebase/firestore';
-import * as React from 'react';
-import { db } from '../../../firebase';
-import Message from './message';
-import SendMessage from './sendmessage';
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import * as React from "react";
+import { db } from "../../../firebase";
+import SendMessage from "./sendmessage";
+import Image from "next/image";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase";
+import profileImage from "../../public/images/profileImage.svg";
 
 export default function ChatBox(props) {
   const [messages, setMessages] = React.useState([]);
+  const [user] = useAuthState(auth);
   const scroll = React.useRef<HTMLInputElement>();
 
   React.useEffect(() => {
     const q = query(
-      collection(db, 'chatRoom', props.roomId, "messages"),
+      collection(db, "chatRoom", props.roomId, "messages"),
       orderBy("createdAt"),
       limit(50)
     );
@@ -30,7 +40,34 @@ export default function ChatBox(props) {
     <main className="chat-box">
       <div className="messages-wrapper">
         {messages?.map((message) => (
-          <Message key={message.id} message={message} />
+          <div
+            key={message.id}
+            className={`chat-bubble ${message.uid === user.uid ? "right" : ""}`}
+          >
+            <div className="message_maindiv">
+              {!message.avatar ? (
+                <Image
+                  className="chat-bubble_img"
+                  src={profileImage}
+                  alt="user avatar"
+                  width={"50%"}
+                  height={"50%"}
+                />
+              ) : (
+                <Image
+                  className="chat-bubble_img"
+                  src={message.avatar}
+                  alt="user avatar"
+                  width={"50%"}
+                  height={"50%"}
+                />
+              )}
+              <p className="user-name">{message.name}</p>
+            </div>
+            <div className="message_div">
+              <span className="user-message">{message.text}</span>
+            </div>
+          </div>
         ))}
         <span ref={scroll}></span>
       </div>
