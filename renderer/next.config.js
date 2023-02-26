716ae618ff2586807ca3fe627bfba0a5ae9aeb08
HyungJinHan <han1210_36@naver.com>
2023-02-27 06:47:29 +0900

    .

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.target = 'electron-renderer';
+      config.target = "electron-renderer";
     }
 
     return config;
+  },
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
   },
 };
