3a37df1c4e21ab2dd4f97773c61dac9ba262bd4a
HyungJinHan <han1210_36@naver.com>
2023-02-27 06:34:10 +0900

    환경변수 처리 완료

--- firebase.ts
+++ firebase.ts
@@ -3,18 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // apiKey: process.env.NEXTRON_APP_CHAT_API_KEY,
-  // authDomain: process.env.NEXTRON_APP_CHAT_AUTH_DOMAIN,
-  // projectId: process.env.NEXTRON_APP_CHAT_PROJECT_ID,
-  // storageBucket: process.env.NEXTRON_APP_CHAT_STORAGE_BUCKET,
-  // messagingSenderId: process.env.NEXTRON_APP_CHAT_MESSSAGING_SENDER_ID,
-  // appId: process.env.NEXTRON_APP_CHAT_APP_ID
-  apiKey: "AIzaSyCiKax-n5OhGOj8X76X8q9VOinwd0WFb94",
-  authDomain: "fir-reactchatapp.firebaseapp.com",
-  projectId: "fir-reactchatapp",
-  storageBucket: "fir-reactchatapp.appspot.com",
-  messagingSenderId: "344586815178",
-  appId: "1:344586815178:web:af055057bbdf96a8fa5633"
+  apiKey: process.env.NEXT_PUBLIC_ENV_FIREBASE_APP_KEY,
+  authDomain: process.env.NEXT_PUBLIC_ENV_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_ENV_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_ENV_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_ENV_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_ENV_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
