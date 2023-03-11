21dd0ef999b03f78951fb31cb555f76901ced58a
HyungJinHan <han1210_36@naver.com>
2023-03-11 20:17:27 +0900

    .

--- renderer/pages/chatroom/chatroom.tsx
+++ renderer/pages/chatroom/chatroom.tsx
@@ -1,30 +1,26 @@
-import { useRouter } from 'next/router';
-import * as React from 'react';
-import styled from 'styled-components';
-import ChatBox from '../chat/chatbox';
-import { chatRoom } from '../../Room';
+import { useRouter } from "next/router";
+import * as React from "react";
+import styled from "styled-components";
+import ChatBox from "../chat/chatbox";
+import { chatRoom } from "../../Room";
 
 const ChatRoomDiv = styled.div`
   text-align: center;
   /* display: inline-block; */
-`
+`;
 
 export default function ChatRoom() {
   const router = useRouter();
   const param = router.asPath;
   const roomId = param.substring(19, 24);
 
-  const room = chatRoom.find(
-    (room) => (
-      room.id === roomId
-    )
-  )
+  const room = chatRoom?.find((room) => room.id === roomId);
 
   return (
     <>
       <ChatRoomDiv>
-        <div className='chatroom_div'>
-          <div className='chatroom_article'>
+        <div className="chatroom_div">
+          <div className="chatroom_article">
             <ChatBox roomId={roomId} />
           </div>
         </div>
